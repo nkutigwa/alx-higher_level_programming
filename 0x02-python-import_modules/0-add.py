3af6abd277fb2428bb7975eb0431ca8dd406c9a0
nkutigwa <nkw802640@gmail.com>
2022-06-03 01:33:42 -0700

    adding task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+!/usr/bin/python3
+
+if __name__ == "__main__":
+    # Import a module
+    import add_0 as sum
+
+# Assign values to variables
+a = 1
+b = 2
+
+# print the result
+print("{} + {} = {}".format(a, b, sum.add(a, b)))
