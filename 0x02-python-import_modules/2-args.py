30adff37350f6ae6d233791c64b5194f7c4a4553
nkutigwa <nkw802640@gmail.com>
2022-06-03 01:36:58 -0700

    adding task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    len_args = len(sys.argv)
+    if len_args == 1:
+        print("{} arguments.".format(len_args - 1))
+    elif len_args == 2:
+        print("{} argument:".format(len_args - 1))
+    else:
+        print("{} arguments:".format(len_args - 1))
+    for i in range(1, len_args):
+        print("{}: {}".format(i, sys.argv[i]))
