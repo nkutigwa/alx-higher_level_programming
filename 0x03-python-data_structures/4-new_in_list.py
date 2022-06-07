288284ab4a701363b85ec82dec143943a41e370d
nkutigwa <nkw802640@gmail.com>
2022-06-06 19:54:40 -0700

    updating task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list[:]
-    else:
-        list_2 = my_list[:]
-        list_2[idx] = element
-        return list_2
+    copy = my_list.copy()
 
+    if idx < 0 or idx >= len(my_list):
+        return copy
+
+    copy[idx] = element
+    return copy
