28c7222d1ecbd9889ad4213db16eebd8652145ab
nkutigwa <nkw802640@gmail.com>
2022-07-07 08:02:38 -0700

    updating task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-"""Square Class
-A Square Class
-"""
+# 2-square.py
+# Carlos Barros <1543@holbertonschool.com>
+"""Size validation: """
 
 
-class Square:
-
+class Square(object):
+    """class variable size"""
     def __init__(self, size=0):
-        """__init__
-        The __init__ method initializes the size value of the square.
-        Attributes:
-            size (:obj:`int`, optional): The size of the square.
-        Raises:
-            TypeError: If `size` type is not `int`.
-            ValueError: If `size` is less than `0`.
-        """
-
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
+        """initialize size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
