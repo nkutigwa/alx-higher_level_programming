2788f6b98ad82f8c5ecc23db081dfa0a741abc7f
nkutigwa <nkw802640@gmail.com>
2022-07-07 07:58:20 -0700

    updating task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-class Square:
+# 1-square.py
+# Carlos Barros <1543@holbertonschool.com>
+"""Private instance attribute: """
+
+
+class Square(object):
+    """class variable size"""
     def __init__(self, size):
+        """initialize size"""
         self.__size = size
