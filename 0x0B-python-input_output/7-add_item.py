bbfcd8dc407bf4da6b7029cf5d951af89766af6a
nkutigwa <nkw802640@gmail.com>
2022-07-08 05:00:16 -0700

    updating task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-"""This module creates a script that adds args to Python list and saves them"""
 
-from os import path
 from sys import argv
+import json
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-if path.exists("add_item.json") is False:
-    save_to_json_file([], "add_item.json")
-my_list = load_from_json_file("add_item.json")
-for i in range(1, len(argv)):
-    my_list.append(argv[i])
-save_to_json_file(my_list, "add_item.json")
+'''
+Write a script that adds all
+arguments to a Python list, and then
+save them to a file
+'''
+
+
+argc = len(argv)
+
+filename = 'add_item.json'
+my_list = []
+try:
+    my_list = load_from_json_file(filename)
+except BaseException:
+    pass
+for items in range(1, argc):
+    my_list.append(argv[items])
+save_to_json_file(my_list, filename)
