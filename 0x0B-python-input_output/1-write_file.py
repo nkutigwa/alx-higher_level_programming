2decdcdd9858ea0add0f479961e1001d81b94591
nkutigwa <nkw802640@gmail.com>
2022-07-08 04:47:25 -0700

    updating task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-'''
-Write a function that returns the number
-of lines of a text file
-'''
+"""This module defines the write_file function"""
 
 
-def number_of_lines(filename=""):
-    line_count = 0
-    with open(filename, encoding='utf-8') as f:
-        for line in f:
-            line_count += 1
-    return line_count
+def write_file(filename="", text=""):
+    """Writes a string to a text file and returns number of characters written
+    Args:
+    filename (str): File to write to
+    text (str): text to write
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(text)
