257ba537a4706ceca72f96d0319ebe5285b904ae
nkutigwa <nkw802640@gmail.com>
2022-07-08 04:45:14 -0700

    updating task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""This module defines the number_of_lines function"""
+'''
+Write a function that returns the number
+of lines of a text file
+'''
 
 
 def number_of_lines(filename=""):
-    """Returns the number of lines of a text file
-    Args:
-    filename (str): Filename
-    """
-    count = 0
-    with open(filename, encoding='utf-8') as file:
-        for line in file:
-            count += 1
-        return count
+    line_count = 0
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
+            line_count += 1
+    return line_count
