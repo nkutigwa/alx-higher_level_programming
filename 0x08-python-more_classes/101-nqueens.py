024d0400f69618308e7385940511b2de42436b96
nkutigwa <nkw802640@gmail.com>
2022-07-08 02:15:23 -0700

    updating task 101

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,19 +1,74 @@
 #!/usr/bin/python3
-""""Module to define a rectangle class"""
+"""queens puzzl
+    """
+
+import sys
 
 
-from sys import argv
+def checkQueen(queens, queen):
+    """Check array queen
+    """
+    for x, y in queens:
+        if y == queen[1]:
+            return False
+        if abs((y - queen[1]) / (x - queen[0])) == 1:
+            return False
+    return True
 
-if len(argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
 
-nqueens = argv[1]
+def placeQueen(n, queens, solutions):
+    """localizate place of queen
+    """
+    if len(queens) == n:
+        for q in queens:
+            solutions.append(q)
 
-if not isinstance(nqueens, int):
-    print('N must be a number')
-    exit(1)
+        return
+    x = len(queens)
+    for y in range(n):
+        queen = [x, y]
+        if checkQueen(queens, queen):
+            queens.append(queen)
+            placeQueen(n, queens, solutions)
+            queens.pop()
 
-if nqueens <= 4:
-    print('N must be at least 4')
-    exit(1)
+
+def validate_args():
+    """Validate value input
+    """
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        sys.exit(1)
+    if n < 4:
+        print('N must be at least 4')
+        sys.exit(1)
+    return n
+
+
+def main():
+    """define program main
+    """
+    n = validate_args()
+    queens = []
+    solutions = []
+    placeQueen(n, queens, solutions)
+
+    arrayaux = []
+
+    cont = 0
+    for m in solutions:
+        cont += 1
+        arrayaux.append(m)
+        if cont == n:
+            cont = 0
+            print(arrayaux)
+            arrayaux = []
+
+
+if __name__ == '__main__':
+    main()
