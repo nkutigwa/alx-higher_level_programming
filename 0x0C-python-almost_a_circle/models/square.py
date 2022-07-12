defb27b4ee76755d861dad4c8a2c1e93fd0223d4
Vicent <51188125+nkutigwa@users.noreply.github.com>
2022-07-12 01:10:41 -0700

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,61 +1,76 @@
 #!/usr/bin/python3
-"""
-This module contains the "Square" class
-"""
-
+'''
+Write the class Square that
+inherits from Rectangle:
+'''
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """A representation of a square"""
+    '''
+    Square class inherits from Rectangle
+    '''
+
     def __init__(self, size, x=0, y=0, id=None):
-        """initializes the square"""
+        '''
+        Constructor
+        '''
         super().__init__(size, size, x, y, id)
         self.size = size
 
     @property
     def size(self):
-        """getter for size"""
+        '''
+        size getter
+        '''
         return self.width
 
     @size.setter
     def size(self, value):
-        """setter for size"""
+        '''
+        size setter
+        '''
         self.width = value
         self.height = value
 
-    def __str__(self):
-        """informal string representation of the square"""
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
-                                                         self.y, self.width)
-
     def update(self, *args, **kwargs):
-        """update attributes"""
-        if len(args):
-            for i, a in enumerate(args):
-                if i == 0:
-                    self.id = a
-                elif i == 1:
-                    self.size = a
-                elif i == 2:
-                    self.x = a
-                elif i == 3:
-                    self.y = a
+        '''
+        Makes args variadic
+        '''
+        argc = len(args)
+        if argc > 0:
+            try:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except BaseException:
+                pass
         else:
-            if "id" in kwargs:
-                self.id = kwargs["id"]
-            if "size" in kwargs:
-                self.size = kwargs["size"]
-            if "x" in kwargs:
-                self.x = kwargs["x"]
-            if "y" in kwargs:
-                self.y = kwargs["y"]
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
 
     def to_dictionary(self):
-        """dictionary representation of a Square"""
-        d = {}
-        d["id"] = self.id
-        d["size"] = self.size
-        d["x"] = self.x
-        d["y"] = self.y
-        return d
+        '''
+       Pull the parameters out in
+       the function as a dictionary
+        '''
+        return {
+            'id': self.id,
+            'x': self.x,
+            'size': self.size,
+            'y': self.y
+        }
+
+    def __str__(self):
+        '''
+        String representation
+        '''
+        return '[Square] ({}) {}/{} - {}'.format(self.id,
+                                                 self.x, self.y, self.size)
