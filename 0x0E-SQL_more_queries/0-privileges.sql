d65cfeca05a93a90533f9696e58b879576e6a97e
nkutigwa <nkw802640@gmail.com>
2022-09-05 03:22:18 -0700

    updating task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- Script that lists all privileges
--- of two users
-SHOW GRANT FOR user_0d_1@localhost;
-SHOW GRANT FOR user_0d_2@localhost;
+-- Privileges of the MySQL users user_0d_1 and user_0d_2å
+-- Lists privileges of the MySQL users user_0d_1 and user_0d_2å
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
