a1b4fe230036780de1149981f641d10c1ed117f0
nkutigwa <nkw802640@gmail.com>
2022-09-05 02:37:04 -0700

    updating task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+// It prints 3 lines
+
+console.log('C is fun\nPython is cool\nJavascript is amazing');
