dd7ceef9c77d05651f0110334e9494ce775d3349
nkutigwa <nkw802640@gmail.com>
2022-09-05 02:52:16 -0700

    updating task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-'use strict';
-let l = process.argv.length;
-if (l === 2) {
-  console.log('No argument');
-} else if (l === 3) {
-  console.log('Argument found');
+let str;
+if (process.argv.length < 3) {
+  str = 'No argument';
+} else if (process.argv.length === 3) {
+  str = 'Argument found';
 } else {
-  console.log('Arguments found');
+  str = 'Arguments found';
 }
+console.log(str);
