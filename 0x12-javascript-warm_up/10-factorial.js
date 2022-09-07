efb8f1318af6bcff46f57d01a0e2d4a598ad5689
nkutigwa <nkw802640@gmail.com>
2022-09-07 03:09:46 -0700

    updating task

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-function factorial (number) {
-  if (isNaN(number)) {
-    return 1;
-  }
-  if (number < 0) {
-    return -1;
-  }
-  if (number === 0) {
-    return 1;
+'use strict';
+let x = process.argv[2];
+function factorial (x) {
+  if (isNaN(x) || x === 1) {
+    return (1);
   } else {
-    return number * factorial(number - 1);
+    return (x * factorial(x - 1));
   }
 }
-
-console.log(factorial(process.argv[2]));
+console.log(factorial(parseInt(x)));
