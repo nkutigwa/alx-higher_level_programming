51664d07f9e3ba14a326c8dd8eab98a013ba3bcd
nkutigwa <nkw802640@gmail.com>
2022-09-07 03:28:23 -0700

    adding task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,26 +1,9 @@
 #!/usr/bin/node
 /*
-  Print the addition of two numbers
+  js
 */
-let myArgc = 0;
-let num1 = 0;
-let num2 = 0;
-
-myArgc = process.argv.length;
-
-num1 = parseInt(process.argv[2]);
-num2 = parseInt(process.argv[3]);
-
-if (myArgc <= 2) {
-  console.log(num1);
-} else if (myArgc === 3 || myArgc === 4) {
-  if (isNaN(num1)) { console.log(num1); } else if (isNaN(num2)) {
-    console.log(num2);
-  } else {
-    console.log(add(num1, num2));
-  }
-}
-
-function add (a, b) {
-  return a + b;
+if (isNaN(parseInt(process.argv[2])) || isNaN(parseInt(process.argv[3]))) {
+  console.log('NaN');
+} else {
+  console.log(parseInt(process.argv[2]) + parseInt(process.argv[3]));
 }
