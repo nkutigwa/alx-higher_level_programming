a3d78af64235ba51b3fd6da160c9d8701485ca2b
nkutigwa <nkw802640@gmail.com>
2022-09-07 02:38:52 -0700

    updating task 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
+/* script that imports a dictionary of occurrences by user id
+and computes a dictionary of user ids by occurrence */
+const dict = require('./101-data').dict;
 const newDict = {};
-let key;
-for (key in dict) {
-  if (dict[key] in newDict) {
-    newDict[dict[key]].push(key);
-  } else {
-    newDict[dict[key]] = [key];
+Object.keys(dict).map(function (key, index) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
   }
-}
+  newDict[dict[key]].push(key);
+});
+
 console.log(newDict);
