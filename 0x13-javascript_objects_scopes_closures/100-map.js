b9104fa95042771c8839a5530b495865649506d9
nkutigwa <nkw802640@gmail.com>
2022-09-07 02:25:20 -0700

    updating task 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-
-const factor = require('./100-data').list;
-
-const map1 = factor.map((index, list1) => index * list1);
-console.log(factor);
-console.log(map1);
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
