3ba7bb6506d0f8e7fc8e2d3c0f334da0893348de
nkutigwa <nkw802640@gmail.com>
2022-09-07 02:36:25 -0700

    updating task 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-//  script that concats 2 files
+
 const fs = require('fs');
-const firstFile = fs.readFileSync(process.argv[2]).toString();
-const secondFile = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], (firstFile + secondFile));
+let content = '';
+
+content = content.concat(fs.readFileSync(process.argv[2]));
+content = content.concat(fs.readFileSync(process.argv[3]));
+fs.writeFileSync(process.argv[4], content);
