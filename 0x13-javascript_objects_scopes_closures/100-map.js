a115b0f3b0be2949accdd78434e5e98d7d81f8f3
nkutigwa <nkw802640@gmail.com>
2022-09-07 02:30:53 -0700

    updating task 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-// imports array from data file; creates new array with map; prints both
-const list = require('./100-data').list;
+const list = require('./100-data.js').list;
+// const newlist = [];
 
+const newlist = list.map((num, index) => {
+  // newlist.push(num * index);
+  return (num * index);
+});
 console.log(list);
-if (Array.isArray(list) && list.every(elem => typeof elem === 'number')) {
-  const newList = list.map((value, index) => value * index);
-  console.log(newList);
-} else {
-  console.log(undefined);
-}
+console.log(newlist);
